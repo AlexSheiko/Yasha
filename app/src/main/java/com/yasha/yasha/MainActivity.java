9ec9180f0d774a1f6379e8e2549e72564f76ee87
Aleksey Sheyko <aleksey_sheyko@hotmail.com>
2015-08-06 23:18:10 +0300

    Initial commit

---
+++ app/src/main/java/com/yasha/yasha/MainActivity.java
@@ -0,0 +1,49 @@
+package com.yasha.yasha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String[] AUTHORS = new String[]{
+            "Marcelle", "Liman", "Katai", "Cambridge"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.message_list_item, R.id.author_textview, AUTHORS);
+
+        ListView messagesList = (ListView) findViewById(R.id.messages_list);
+        messagesList.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
